de469fd8e0cdb7f4a1f8906d1faed6250bae007d
Fabian Pedregosa <fabian@fseoane.net>
2013-05-09 15:48:06 +0200

    initial commit

---
+++ pytron/_tron.pyx
@@ -0,0 +1,73 @@
+import numpy as np
+cimport numpy as np
+
+from libc cimport string
+
+cdef extern from "tron_helper.h":
+    ctypedef void (*f_cb)(double *, void *, double *, int)
+    ctypedef void (*grad_cb)(double *, double *)
+    ctypedef void (*hess_cb)(double *, double *)
+    # cdef cppclass func_callback:
+    #     func_callback(void *f_py, f_cb c_func, grad_cb c_grad, 
+    #         hess_cb c_hess, int nr_variable)
+    #     double fun(double *)
+    #     void grad(double *, double *)
+    #     void Hv(double *, double *)
+    #     int get_nr_variable()       
+
+cdef extern from "tron.h":
+    cdef cppclass function:
+        double fun(double *w)
+        void grad(double *w, double *g)
+        void Hv(double *s, double *Hs)
+        int get_nr_variable()
+
+    cdef cppclass TRON:
+        TRON(function *, double, int)
+        tron(double *)
+
+cdef double c_func(double *w, void *f_py, double *out, int nr_variable):
+  w_np = np.empty(nr_variable, dtype=np.float64)
+  string.memcpy(<void *> w_np.data, w, nr_variable * sizeof(double))
+  out[0] = <double> (<object> f_py)(w_np)
+
+
+def minimize(f, grad, hess, x0, *args):
+    """
+    f : callable
+        f(w, *args)
+    grad: callable
+    hess: callable
+    """
+
+    x0 = np.asarray(x0)
+    cdef int nr_variable = x0.size
+
+    cdef np.ndarray[np.float64_t, ndim=1] x0_np
+    cdef np.ndarray[np.float64_t, ndim=1] w_np
+    #cdef np.ndarray[np.float64_t, ndim=1] g_np
+    #cdef np.ndarray[np.float64_t, ndim=1] s_np
+    cdef np.ndarray[np.float64_t, ndim=1] Hs_np
+
+    x0_np = np.asarray(x0)
+    w_np = np.empty(x0.size, dtype=np.float64)
+    Hs_np = np.empty(x0.size * x0.size, dtype=np.float64)
+
+    f_py = lambda x: f(x, *args)
+    cdef double *out
+    c_func(<double *> w_np.data, <void *> f_py, out, nr_variable)
+
+
+    # cdef void c_grad(double *w, double *g):
+    #   # memcpy could be avoided
+    #   string.memcpy(w_np.data, w, x0.size * sizeof(double))
+    #   g_np = grad(w_np, *args)
+    #   string.memcpy(g, g_np.data, x0.size * sizeof(double))
+
+    # cdef void c_hess(double *s, double *Hs):
+    #   s_np = np.empty(x0.size, dtype=np.float64)
+    #   string.memcpy(s_np.data, s, x0.size * sizeof(double))
+    #   hess_np = hess(s_np, *args)
+    #   string.memcpy(Hs, Hs_np, Hs_np.size * sizeof(double))
+
+
---
+++ setup.py
@@ -0,0 +1,45 @@
+import pytron
+from distutils.core import setup, Extension
+from Cython.Distutils import build_ext
+import numpy as np
+from glob import glob
+
+CLASSIFIERS = """\
+Development Status :: 5 - Production/Stable
+Intended Audience :: Science/Research
+Intended Audience :: Developers
+License :: OSI Approved
+Programming Language :: Python
+Programming Language :: Python :: 2
+Programming Language :: Python :: 2.6
+Programming Language :: Python :: 2.7
+Programming Language :: Python :: 3
+Programming Language :: Python :: 3.2
+Programming Language :: Python :: 3.3
+Topic :: Software Development
+Operating System :: POSIX
+Operating System :: Unix
+
+"""
+
+sources =['pytron/_tron.pyx', 'pytron/src/tron.cpp'] + glob('pytron/src/blas/*.c')
+
+
+setup(
+    name='pytron',
+    description='Python bindings for TRON optimizer',
+    long_description=open('README.rst').read(),
+    version=pytron.__version__,
+    author='Fabian Pedregosa',
+    author_email='f@fabianp.net',
+    url='http://pypi.python.org/pypi/pytron',
+    packages=['pytron'],
+    classifiers=[_f for _f in CLASSIFIERS.split('\n') if _f],
+    license='Simplified BSD',
+    requires = ['numpy', 'scipy'],
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension('pytron._tron',
+        sources=sources,
+        language='c++', include_dirs=[np.get_include(), 'pytron/src/'])],
+
+)
