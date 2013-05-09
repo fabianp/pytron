5996017bb88818fa76148d4c3596e2ea401df2cb
Fabian Pedregosa <fabian@fseoane.net>
2013-05-09 23:05:14 +0200

    more

--- pytron/_tron.pyx
+++ pytron/_tron.pyx
@@ -1,7 +1,8 @@
+from cython cimport view
 import numpy as np
 cimport numpy as np
 from libc cimport string
-from python_ref cimport Py_INCREF, Py_DECREF
+#from python_ref cimport Py_INCREF, Py_DECREF
 
 np.import_array()
 
@@ -18,34 +19,44 @@ cdef extern from "tron.h":
         TRON(func_callback *, double, int)
         void tron(double *)
 
-cdef void c_func(double *w, void *f_py, double *b, int nr_variable) with gil:
-    cdef np.ndarray[np.float64_t, ndim=1] x0_np
-    x0_np = np.empty(nr_variable, dtype=np.float64)
-    string.memcpy(<void *> x0_np.data, <void *> w, nr_variable * sizeof(double))
-    b[0] = (<object> f_py)(x0_np)
+cdef void c_func(double *w, void *f_py, double *b, int nr_variable):
+    # 1. convert w to numpy array
+    # 2. call f_py
+    # 3. get the result
+    # 4. free temp objects
+    cdef view.array w0 = view.array(shape=(nr_variable,), itemsize=sizeof(double),
+        mode='c', format='d', allocate_buffer=False)
+    w0.data = <char *> w
+    b[0] = (<object> f_py)(w0)
+    w0 = None
+    del w0
 
 
-cdef void c_grad(double *w, void *f_py, double *b, int nr_variable) with gil:
-    cdef np.ndarray[np.float64_t, ndim=1] g_np
-    cdef np.ndarray[np.float64_t, ndim=1] x0_np
-    x0_np = np.zeros(nr_variable, dtype=np.float64)
-    string.memcpy(<void *> x0_np.data, <void *> w, nr_variable * sizeof(double))
-    out = (<object> f_py)(x0_np)
-    g_np = np.asarray(out)
-    assert g_np.size == nr_variable
-    string.memcpy(<void *> b, <void *> g_np.data, nr_variable * sizeof(double))
+cdef void c_grad(double *w, void *f_py, double *b, int nr_variable):
+    cdef view.array b0 = view.array(shape=(nr_variable,), itemsize=sizeof(double),
+        mode='c', format='d', allocate_buffer=False)
+    b0.data = <char *> b
+    cdef view.array w0 = view.array(shape=(nr_variable,), itemsize=sizeof(double),
+        mode='c', format='d', allocate_buffer=False)
+    w0.data = <char *> w
+#    cdef view.array w0 = <double[:nr_variable]> w
+    out = (<object> f_py)(w0)
+    b0[...] = out
+    del b0, w0, out
 
 
-cdef void c_hess(double *w, void *f_py, double *b, int nr_variable) with gil:
-    cdef np.ndarray[np.float64_t, ndim=1] Hs_np
-    cdef np.ndarray[np.float64_t, ndim=1] x0_np
-    x0_np = np.empty(nr_variable, dtype=np.float64)
-    string.memcpy(<void *> x0_np.data, <void *> w, nr_variable * sizeof(double))
-    out = (<object> f_py)(x0_np)
-    Hs_np = np.asarray(out).ravel('C')
-    assert Hs_np.size == nr_variable * nr_variable
-    string.memcpy(<void *> b, <void *> Hs_np.data, 
-        nr_variable * nr_variable * sizeof(double))
+cdef void c_hess(double *w, void *f_py, double *b, int nr_variable):
+    cdef view.array b0 = view.array(shape=(nr_variable * nr_variable,), 
+        itemsize=sizeof(double), format='d',
+        mode='c', allocate_buffer=False)
+    b0.data = <char *> b
+    cdef view.array w0 = view.array(shape=(nr_variable,), 
+        itemsize=sizeof(double), format='d',
+        mode='c', allocate_buffer=False)
+    w0.data = <char *> w
+    out = (<object> f_py)(w0)
+    b0[:] = out
+    del b0, w0, out
 
 def minimize(f, grad, hess, x0, args=(), max_iter=1000, tol=1e-6):
     """
@@ -62,13 +73,18 @@ def minimize(f, grad, hess, x0, args=(), max_iter=1000, tol=1e-6):
     cdef int c_max_iter = max_iter
 
     def py_func(w):
-        return f(w, *args)
+        w0 = np.asarray(w)
+        return f(w0, *args)
 
     def py_grad(w):
-        return grad(w, *args)
+        w0 = np.asarray(w)        
+        out = grad(w0, *args)
+        return out.astype(np.float64).ravel()
 
     def py_hess(w):
-        return hess(w, *args)
+        w0 = np.asarray(w)
+        out = hess(w0, *args)
+        return out.astype(np.float64).ravel()
 
     cdef func_callback * fc = new func_callback(
         <void *> py_func, c_func, 
@@ -76,14 +92,12 @@ def minimize(f, grad, hess, x0, args=(), max_iter=1000, tol=1e-6):
         <void *> py_hess, c_hess, nr_variable)
 
     cdef TRON *solver = new TRON(fc, c_tol, c_max_iter)
-    print('const')
     solver.tron(<double *> x0_np.data)
-    print('out')
 
 #    del fc
 #    del solver
-    # import sys
-    # print sys.getrefcount(py_func)
+    import sys
+    print sys.getrefcount(py_func)
     # print sys.getrefcount(py_grad)
 
     return x0_np
